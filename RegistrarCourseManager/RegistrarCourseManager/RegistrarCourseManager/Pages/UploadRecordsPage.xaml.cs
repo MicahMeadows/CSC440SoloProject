bdee5443306c704aa74eb58fa538f05696cd0c1b
MicahMeadows <micahstlp@gmail.com>
2021-10-14 13:29:05 -0400

    Add popup window

--- RegistrarCourseManager/RegistrarCourseManager/RegistrarCourseManager/Pages/UploadRecordsPage.xaml.cs
+++ RegistrarCourseManager/RegistrarCourseManager/RegistrarCourseManager/Pages/UploadRecordsPage.xaml.cs
@@ -5,13 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using RegistrarCourseManager.Windows;
 
 namespace RegistrarCourseManager.Pages
 {
@@ -23,6 +17,12 @@ namespace RegistrarCourseManager.Pages
         public UploadRecordsPage()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            CourseWindow popup = new CourseWindow();
+            popup.ShowDialog();
         }
     }
 }
