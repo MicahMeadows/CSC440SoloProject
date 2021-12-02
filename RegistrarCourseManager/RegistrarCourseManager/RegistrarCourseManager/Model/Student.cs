e37c52f6b352a330f888f20ddfdda956254bf04d
MicahMeadows <micahstlp@gmail.com>
2021-12-02 00:24:20 -0500

    Style student listview

---
+++ RegistrarCourseManager/RegistrarCourseManager/RegistrarCourseManager/Model/Student.cs
@@ -0,0 +1,16 @@
+﻿namespace RegistrarCourseManager.Model
+{
+    public class Student
+    {
+        public string StudentID { get; set; }
+        public string Name { get; set; }
+        public double OverallGPA { get; set; }
+
+        public Student(string StudentID, string Name, double OverallGPA)
+        {
+            this.StudentID = StudentID;
+            this.Name = Name;
+            this.OverallGPA = OverallGPA;
+        }
+    }
+}
--- RegistrarCourseManager/RegistrarCourseManager/RegistrarCourseManager/ViewModel/Tabs/StudentSearchViewModel.cs
+++ RegistrarCourseManager/RegistrarCourseManager/RegistrarCourseManager/ViewModel/Tabs/StudentSearchViewModel.cs
@@ -1,13 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using RegistrarCourseManager.Model;
 
 namespace RegistrarCourseManager.ViewModel.Tabs
 {
     class StudentSearchViewModel : ViewModelBase
     {
+        private Student selectedStudent;
+        public Student SelectedStudent
+        {
+            get => selectedStudent;
+            set
+            {
+                selectedStudent = value;
+                OnPropertyChanged("SelectedStudent");
+            }
+        }
 
+        private ObservableCollection<Student> students = new ObservableCollection<Student>();
+        public ObservableCollection<Student> Students
+        {
+            get => students;
+            set
+            {
+                students = Students;
+                OnPropertyChanged("Students");
+            }
+        }
+
+        public StudentSearchViewModel()
+        {
+            Students.Add(new Student(Name: "Micah", StudentID: "901901901", OverallGPA: 2.5));
+            Students.Add(new Student(Name: "Micah2", StudentID: "901901902", OverallGPA: 2.6));
+            Students.Add(new Student(Name: "Micah3", StudentID: "901901903", OverallGPA: 2.7));
+        }
     }
 }
